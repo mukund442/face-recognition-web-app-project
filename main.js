67a3d3a6bee55ac419aba5cf3e6aa77ea1b65795
mukund442 <92722963+mukund442@users.noreply.github.com>
2022-03-28 14:41:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ Webcam.set({
     width:350,
     height:300,
     image_format:'png',
-    quality:90
+    quality:2
 });
 
 camera=document.getElementById("camera");
@@ -18,3 +18,24 @@ function take_snapshot(){
 
 console.log("ml5 version:", ml5.version);
 classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/[...]', modelLoaded);
+
+function modelLoaded(){
+    console.log('model loaded');
+}
+
+function check(){
+    img=document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error)
+    }
+    else{
+        console.log(results)
+        document.getElementById("result_object_name").innerHTML=results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML=results[0].confidene;
+    }
+}
